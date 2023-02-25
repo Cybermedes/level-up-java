ccea4b2fefd2a80ab27cee98fa287798ceb6923e
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-02-24 22:52:03 -0300

    Added a new program file to the Exercises part 2

---
+++ src/Exercises part 2/QuadraticEquation.java
@@ -0,0 +1,8 @@
+public class QuadraticEquation {
+
+    public static void main(String[] args) {
+
+        System.out.println("The quadratic equation: ");
+        System.out.println("a*x^2 + b*x + c = 0");
+    }
+}
