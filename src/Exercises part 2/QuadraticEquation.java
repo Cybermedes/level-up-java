19287eed8754514c09acf7ee809905fdde600004
Lucas Paiva <115654192+Cybermedes@users.noreply.github.com>
2023-02-24 22:57:20 -0300

    Delete src/Exercises part 2 directory

--- src/Exercises part 2/QuadraticEquation.java
+++
@@ -1,8 +0,0 @@
-public class QuadraticEquation {
-
-    public static void main(String[] args) {
-
-        System.out.println("The quadratic equation: ");
-        System.out.println("a*x^2 + b*x + c = 0");
-    }
-}
