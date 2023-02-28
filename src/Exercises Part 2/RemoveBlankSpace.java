bf3b828be235b0b09911f69fe92d89616fee7dff
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-02-27 21:25:36 -0300

    Added more exercises to the folder Exercises Part 2

---
+++ src/Exercises Part 2/RemoveBlankSpace.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class RemoveBlankSpace {
+
+    //Program that removes the blank space of a string typed by the user
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        String userText = reader.nextLine();
+
+        System.out.println("The original text: \"" + userText + "\"");
+        userText = userText.replaceAll("\\s", "");
+        System.out.println("The text without blank spaces: \"" + userText + "\"");
+    }
+}
