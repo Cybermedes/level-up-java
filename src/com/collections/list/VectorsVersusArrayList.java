04aba2417371871629b5bee698554b11013d78ac
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-06-07 23:29:09 -0300

    Add multithreads for comparison

--- src/com/collections/list/VectorsVersusArrayList.java
+++ src/com/collections/list/VectorsVersusArrayList.java
@@ -6,8 +6,7 @@ import java.util.Vector;
 
 class VectorsVersusArrayList {
 
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		final int size = 1_000_000;
 
@@ -38,5 +37,57 @@ class VectorsVersusArrayList {
 		end = System.currentTimeMillis();
 		System.out.println("Added " + size + " elements to the Vector: " 
 		+ (end - start) + " ms");
+		
+		//Create 2 ArrayList threads to to run at the same time
+		List<Integer> multithreadedList = new ArrayList<>();
+		start = System.currentTimeMillis();
+		
+		Thread thread1 = new Thread(() -> {
+			for (int i = 0; i < size; i++) {
+				multithreadedList.add(i);
+			}
+		});
+		
+		Thread thread2 = new Thread(() -> {
+			for (int i = 0; i < size; i++) {
+				multithreadedList.add(i);
+			}
+		});
+		
+		thread1.start();
+		thread2.start();
+		thread1.join();
+		thread2.join();
+		
+		end = System.currentTimeMillis();
+		System.out.println("\nAdded elements using multithreading to the ArrayList: " 
+		+ (end - start) + " ms");
+		System.out.println("Elements added: " + multithreadedList.size());
+		
+		//Create 2 Vector threads to to run at the same time
+		List<Integer> multithreadedVector = new Vector<>();
+		start = System.currentTimeMillis();
+		
+		thread1 = new Thread(() -> {
+			for (int i = 0; i < size; i++) {
+				multithreadedVector.add(i);
+			}
+		});
+		
+		thread2 = new Thread(() -> {
+			for (int i = 0; i < size; i++) {
+				multithreadedVector.add(i);
+			}
+		});
+		
+		thread1.start();
+		thread2.start();
+		thread1.join();
+		thread2.join();
+		
+		end = System.currentTimeMillis();
+		System.out.println("Added elements using multithreading to the Vector: " 
+		+ (end - start) + " ms");
+		System.out.println("Elements added: " + multithreadedVector.size());
 	}
 }
