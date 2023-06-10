aefb2023aea62d171e6f8a8ea9306b4560415c54
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-06-09 23:07:39 -0300

    Create a class that implements the queue data structure

---
+++ src/com/collections/queue/CollectionsQueue.java
@@ -0,0 +1,33 @@
+package com.collections.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class CollectionsQueue {
+
+	public static void main(String[] args) {
+		
+		//Creating a queue from the Queue Interface
+		Queue<String> queue = new LinkedList<>();
+		
+		queue.offer("Karen");
+		queue.offer("Chad");
+		queue.offer("Steve");
+		queue.offer("Harold");
+		
+		//Print the elements of the queue
+		System.out.println(queue);
+		
+		//Retrieves the head of the queue
+		System.out.println("The head of the queue: " + queue.peek() + "\n");
+		
+		//Removes the head of the queue
+		String head = queue.poll();
+		System.out.println("Remove the head of the queue: " + head);
+		System.out.println(queue);
+		
+		//Returns the size of the queue
+		System.out.println("Size of the queue: " + queue.size() + "\n");
+		
+	}
+}
